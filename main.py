b8822a46caa010fd8b69568dbcef05a89a0e5bfd
VesperaBet <romainjouary@gmail.com>
2025-03-26 15:29:32 +0100

    main.py

--- main.py
+++ main.py
@@ -143,5 +143,8 @@ def main():
     return {"status": "Analyse hockey en cours"}, 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
 
